b1df501d11ce1a485092c0a99652e2f7d075f170
Barath-tech <barathsundar03@gmail.com>
2023-11-10 12:03:28 +0530

    Initial configuration and authentication was added

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full justify-center flex items-center">{children}</div>
+  );
+};
+
+export default AuthLayout;
